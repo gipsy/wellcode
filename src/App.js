a0ab71ebed52c10852cbefbe9b3c87ea56efe619
gipsy <ter4444@gmail.com>
2023-04-20 13:07:52 +0300

    started on Contact section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Services from "./components/services/Services";
 import Qualification from "./components/qualification/Qualification";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonials />
+        <Contact />
       </main>
     </>
   )
